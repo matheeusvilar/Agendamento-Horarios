97953b61b683e2a4dd5d19bef2ac1aae6a8647da
matheeusvilar <matheus.vilar@gmail.com>
2026-02-20 02:17:51 -0300

    Terminei  verificao de conflito de agendamento

--- src/main/java/com/javanauta/agendador_horarios/infrastructure/repository/AgendamentoRepository.java
+++ src/main/java/com/javanauta/agendador_horarios/infrastructure/repository/AgendamentoRepository.java
@@ -3,6 +3,9 @@ package com.javanauta.agendador_horarios.infrastructure.repository;
 import com.javanauta.agendador_horarios.infrastructure.entity.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
 
+    Agendamento findByServicoAndDataHoraAgendamentoBetween(String servico, LocalDateTime dataHoraInicio, LocalDateTime dataHoraFinal);
 }
--- src/main/java/com/javanauta/agendador_horarios/services/AgendamentoService.java
+++ src/main/java/com/javanauta/agendador_horarios/services/AgendamentoService.java
@@ -1,12 +1,32 @@
 package com.javanauta.agendador_horarios.services;
 
+import com.javanauta.agendador_horarios.infrastructure.entity.Agendamento;
 import com.javanauta.agendador_horarios.infrastructure.repository.AgendamentoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor //INJECAO DE DEPENDENCIAS
 public class AgendamentoService {
 
     private final AgendamentoRepository agendamentoRepository;
+
+    //VERIFICANDO SE A DATA E HORA JA NAO ESTA MARCADA PARA OUTRA PESSOA
+
+    public Agendamento salvarAgendamento(Agendamento agendamento){
+
+        LocalDateTime horaAgendamento = agendamento.getDataHoraAgendamento();
+        LocalDateTime horaFim = agendamento.getDataHoraAgendamento().plusHours(1);
+
+        Agendamento agendados = agendamentoRepository.findByServicoAndDataHoraAgendamentoBetween(agendamento.getServico(), horaAgendamento,horaFim);
+
+        if (Objects.nonNull(agendados)){
+            throw new RuntimeException("Horario já está preenchido");
+        }
+        return agendamentoRepository.save(agendamento);
+
+    }
 }
